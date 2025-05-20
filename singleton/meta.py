f8a2b85ae480028493abc8485937312957568e7c
Szymon Domżalski <100307341+szdomzalski@users.noreply.github.com>
2025-05-20 23:15:37 +0200

    Testing overriding __new__ and __call__. Improved comments and docstrings.

--- singleton/meta.py
+++ singleton/meta.py
@@ -4,7 +4,7 @@ from threading import Lock
 class SingletonMetaLazy(type):
     """
     Singleton metaclass that ensures only one instance of a class is created.
-    This version creates the instance only when it is first accessed.
+    This version creates the instance only when it is first accessed (lazy instantiation).
     This is useful for classes that may not be used immediately or at all.
     """
     __instances = {}
@@ -12,13 +12,17 @@ class SingletonMetaLazy(type):
 
     def __call__(cls, *args, **kwargs):
         """
-        This method is called when an instance of the class is created.
+        This method is called when an instance of the class is requested.
         It checks if an instance already exists. If it does, it returns that
         instance. If not, it creates a new instance and stores it in the
         __instances dictionary.
+
+        This method takes over the default __call__ method of the class - it means overriding __call__ method in the
+        called cls will not work (that method will not be called).
         """
         with cls.__lock:  # Ensure thread safety
             if cls not in cls.__instances:
+                # Invoking __call__ of "type" to create the instance like in the default __call__ method
                 cls.__instances[cls] = super().__call__(*args, **kwargs)
 
         return cls.__instances[cls]
@@ -28,8 +32,7 @@ class SingletonMetaEager(type):
     """
     Singleton metaclass that ensures only one instance of a class is created.
     This version creates the instance at the time of class definition.
-    This is useful for classes that are always needed and should be created
-    immediately.
+    This is useful for classes that are always needed and should be created immediately.
     """
     __instances = {}
 
@@ -38,14 +41,19 @@ class SingletonMetaEager(type):
         This method is called when the class is created. It creates a new
         instance of the class and stores it in the __instances dictionary.
         """
+        # Invoke the __new__ method of the parent class (type) to create the class like in the default __new__ method
         new_cls = super().__new__(cls, *args, **kwargs)
-        cls.__instances[new_cls] = super(SingletonMetaEager, new_cls).__call__()
+
+        # Both below calls are equivalent in effect
+        # The first line uses method-wrapper to call the __call__ method of the parent class (type)
+        # The seconde one uses the __call__ method of the parent class (type) directly - slot wrapper approach
+        # cls.__instances[new_cls] = super(SingletonMetaEager, new_cls).__call__(start=25)
+        cls.__instances[new_cls] = super().__call__(new_cls, start=25)
         return new_cls
 
     def __call__(cls, *args, **kwargs):
         """
-        This method is called when an instance of the class is created.
-        It returns the instance created at the time of
-        class definition.
+        This method is called when an instance of the class is requested.
+        It returns the instance created at the time of class definition.
         """
         return cls.__instances[cls]
--- singleton/sequence_generator.py
+++ singleton/sequence_generator.py
@@ -7,8 +7,7 @@ class SequenceGeneratorLazy(metaclass=SingletonMetaLazy):
     """
     A class that generates a sequence of numbers.
     This class is a singleton, meaning only one instance of it can exist.
-    The instance is created when the class is first accessed.
-    This is useful for classes that may not be used immediately or at all.
+    The instance is created when the class is first time accessed.
     The sequence starts from a given number and increments by 1 each time
     the next number is requested.
     The class is thread-safe, meaning it can be used in a multi-threaded
@@ -17,43 +16,35 @@ class SequenceGeneratorLazy(metaclass=SingletonMetaLazy):
 
     def __init__(self, start: int = 0) -> None:
         """
-        Initializes the sequence generator with a starting number.
+        Initializes the sequence generator with a starting number. The initialization takes place only once,
+        when the class is first accessed. Every subsequent access to the class will return the same instance, so the
+        initialization will not be called again and the current number will not be reinitialized.
+
         :param start: The starting number of the sequence.
+        :return: None
         """
         self.current = start
         self.__lock = Lock()
 
-
-    def get_next_number(self) -> int:
+    def __new__(cls, *args, **kwargs):
         """
-        Returns the next number in the sequence.
-        :return: The next number in the sequence.
+        This method is called when the class is first instantiated. This method is called with arguments from class call
+        when requesting an instance in the metaclass but super().__new__ has to be called without arguments to work
+        properly.
+        This method has been overridden in this class just for showing the example of how to do it with singleton meta.
+        :param args: Arguments passed to the class.
+        :param kwargs: Keyword arguments passed to the class.
+        :return: The instance of the class.
         """
-        with self.__lock:  # Ensure thread safety
-            # Increment the current number and return it
-            self.current += 1
-            return self.current
+        return super().__new__(cls)
 
-
-class SequenceGeneratorEager(metaclass=SingletonMetaEager):
-    """
-    A class that generates a sequence of numbers.
-    This class is a singleton, meaning only one instance of it can exist.
-    The instance is created at the time of class definition.
-    This is useful for classes that are always needed and should be created
-    immediately.
-    The sequence starts from a given number and increments by 1 each time
-    the next number is requested.
-    The class is thread-safe, meaning it can be used in a multi-threaded
-    environment without causing data corruption.
-    """
-
-    def __init__(self, start: int = 0) -> None:
+    def __call__(cls, *args, **kwargs):
         """
-        Initializes the sequence generator.
+        In current scenario this method is not used at all. It is overridden just as a demonstration that even "pass"
+        won't break anything.
         """
-        self.current = start
-        self.__lock = Lock()
+        pass
+
 
     def get_next_number(self) -> int:
         """
@@ -61,7 +52,55 @@ class SequenceGeneratorEager(metaclass=SingletonMetaEager):
         :return: The next number in the sequence.
         """
         with self.__lock:
-            # Increment the current number and return it
+            self.current += 1
+            return self.current
+
+class SequenceGeneratorEager(metaclass=SingletonMetaEager):
+    """
+    A class that generates a sequence of numbers.
+    This class is a singleton, meaning only one instance of it can exist.
+    The instance is created at the time of class definition.
+    The sequence starts from a given number and increments by 1 each time
+    the next number is requested.
+    The class is thread-safe, meaning it can be used in a multi-threaded
+    environment without causing data corruption.
+    """
+
+    def __init__(self, start: int = 0) -> None:
+        """
+        Initializes the sequence generator. The initialization takes place at the time of class definition because of
+        eager instantiation. The current number is initialized to the starting number specified in the metaclass
+        __new__ method so eager instantiation prefers no arguments to be passed in the class call.
+        :param start: The starting number of the sequence.
+        :return: None
+        """
+        self.current = start
+        self.__lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        """
+        This method is called when the class is defined. This method is called with arguments from the metaclass call
+        but super().__new__ has to be called without arguments to work properly.
+        This method has been overridden in this class just for showing the example of how to do it with singleton meta.
+        :param args: Arguments passed to the class.
+        :param kwargs: Keyword arguments passed to the class.
+        :return: The instance of the class.
+        """
+        return super().__new__(cls)
+
+    def __call__(cls, *args, **kwargs):
+        """
+        In current scenario this method is not used at all. It is overridden just as a demonstration that even "pass"
+        won't break anything.
+        """
+        pass
+
+    def get_next_number(self) -> int:
+        """
+        Returns the next number in the sequence.
+        :return: The next number in the sequence.
+        """
+        with self.__lock:
             self.current += 1
             return self.current
 
@@ -73,18 +112,18 @@ if __name__ == "__main__":
     print(seq_gen.get_next_number())  # Output: 12
 
     # This is singleton, so it won't create a new instance and won't change the current value
-    seq_gen2 = SequenceGeneratorLazy()
+    seq_gen2 = SequenceGeneratorLazy(start=20)
     print(id(seq_gen) == id(seq_gen2))  # Output: True
-    print(seq_gen.get_next_number())
-    print(seq_gen.get_next_number())
+    print(seq_gen.get_next_number())  # Output: 13
+    print(seq_gen.get_next_number())  # Output: 14
 
     # Keyword argument is ignored because of eager instatiation - it should be passed in the metaclass
-    seq_gen_eager = SequenceGeneratorEager(start=20)
-    print(seq_gen_eager.get_next_number())  # Output: 1
-    print(seq_gen_eager.get_next_number())  # Output: 2
+    seq_gen_eager = SequenceGeneratorEager(start=10)
+    print(seq_gen_eager.get_next_number())  # Output: 26
+    print(seq_gen_eager.get_next_number())  # Output: 27
 
     # This is singleton, so it won't create a new instance and won't reinitialize the current value
     seq_gen_eager2 = SequenceGeneratorEager()
     print(id(seq_gen_eager) == id(seq_gen_eager2))  # Output: True
-    print(seq_gen_eager.get_next_number())
-    print(seq_gen_eager.get_next_number())
+    print(seq_gen_eager.get_next_number())  # Output: 28
+    print(seq_gen_eager.get_next_number())  # Output: 29
